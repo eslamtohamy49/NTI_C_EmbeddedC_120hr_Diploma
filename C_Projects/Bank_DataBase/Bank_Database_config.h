1555426807a491ee752e1b98bc428984ea6d3e31
Eslam Tohamy <117383087+eslamtohamy49@users.noreply.github.com>
2023-12-30 22:16:27 +0200

    Add files via upload

---
+++ C_Projects/Bank_DataBase/Bank_Database_config.h
@@ -0,0 +1,24 @@
+#ifndef BanK_Database_config_H
+#define BanK_Database_config_H
+
+
+
+#define CONFIG_ENABLE        0x01
+#define CONGIG_DISABLE       0x00
+
+
+
+
+#define EDIT_CUSTOMER_DATA_CONFIGURATION          CONFIG_ENABLE
+#define CREATE_NEW_CUSTOMER_CONFIGURATION         CONFIG_ENABLE
+#define PRINT_CUSTOMER_DATA_CONFIGURATION         CONFIG_ENABLE
+#define DELETE_CUSTOMER_DATA_CONFIGURATION        CONFIG_ENABLE
+#define TRANSFER_CUSTOMER_MONEY_CONFIGURATION     CONFIG_ENABLE
+
+
+
+
+
+
+
+#endif // BanK_Database_config_H
